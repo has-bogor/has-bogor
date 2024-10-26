5f082d3a13502693d525c84d405b843a5f61bc77
Michael Ignasius <michael.ignasius@ui.ac.id>
2024-10-26 20:59:08 +0700

    menambahkan models.py

--- penyimpanan/models.py
+++ penyimpanan/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# from category.models import Category
 
 # Create your models here.
+
+class Katalog(models.Model):
+    id = models.AutoField(primary_key=True)
+    nama = models.CharField(max_length=100)
+    kategori = models.CharField(max_length=100)
+    # kategori = models.ManyToManyField(Category, on_delete=models.CASCADE)
+    harga = models.IntegerField()
+    deskripsi = models.TextField()
+    toko = models.CharField(max_length=100)
