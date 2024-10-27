387416c4000f398be83d40b95f8c6f13421e5da5
IvanJehuda <ivanjangi@gmail.com>
2024-10-27 16:29:28 +0700

    adding urls.py

--- has_bogor/settings.py
+++ has_bogor/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'authentication',
     'penyimpanan',
     'pembayaran',
+    'category'
 ]
 
 MIDDLEWARE = [
--- has_bogor/urls.py
+++ has_bogor/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pembayaran/', include('pembayaran.urls')),
+    path('category/', include('category.urls'))
 ]
