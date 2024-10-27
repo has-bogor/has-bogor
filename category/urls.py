2e084c02c7bd91c821b54fc13a0f4a520f452196
IvanJehuda <ivanjangi@gmail.com>
2024-10-27 20:17:16 +0700

    fixing template

--- category/urls.py
+++ category/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories/', views.list_categories_ordered, name='list_categories_ordered'),  
-    path('categories/create/', views.create_category, name='create_category'),  
-    path('categories/update/<int:id>/', views.update_category, name='update_category'),  
-    path('categories/delete/<int:id>/', views.delete_category, name='delete_category'),  
-
+    path('', views.category_management, name='category_management'),  # This is the main view
+    path('categories/', views.list_categories_ordered, name='list_categories_ordered'),
+    path('categories/create/', views.create_category, name='create_category'),
+    path('categories/update/<int:id>/', views.update_category, name='update_category'),
+    path('categories/delete/<int:id>/', views.delete_category, name='delete_category'),
 ]
