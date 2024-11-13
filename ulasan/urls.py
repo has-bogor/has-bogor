a4b386c4adac424a4c0d3d7e28ff313992d4360b
IsmailYanuar <ismailyanuar1960@gmail.com>
2024-11-13 22:54:09 +0700

    test

--- ulasan/urls.py
+++ ulasan/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('show_ulasan', show_ulasan, name='show_ulasan'),  
     path('delete/<int:pk>/', delete_ulasan, name='delete_ulasan'),  
     path('edit/<int:pk>/', edit_ulasan, name='edit_ulasan'), 
+    path('edit-ulasan/<int:pk>/', edit_ulasan, name='edit_ulasan_alt'),
     path('json/', show_json, name='show_json'),  
     path('list_json/', add_ulasan_ajax, name='list_ulasan_json'),  
     path('list-ulasan-ajax', add_ulasan_ajax, name='add_ulasan_ajax')
--- ulasan/views.py
+++ ulasan/views.py
@@ -24,9 +24,10 @@ from penyimpanan.models import Katalog  # Ensure your Katalog model is imported
 #     return render(request, "list_ulasan.html", context)
 
 def show_ulasan(request):
-    ulasans = Ulasan.objects.all()  # Ambil semua ulasan dari database
+    ulasan = Ulasan.objects.all()
+    ulasan_list = list(ulasan.values('id', 'ulasan_makanan_souvenir__nama', 'rating', 'pesan_ulasan'))
     context = {
-        'ulasans': ulasans,
+        'ulasans': ulasan_list,
     }
     return render(request, 'list_ulasan.html', context)
 
@@ -79,18 +80,24 @@ def create_ulasan(request):
 #     return render(request, 'create_ulasan.html', {'form': form})
 
 
-def edit_ulasan(request, id):
-    ulasan = get_object_or_404(Ulasan, pk=id)
+def edit_ulasan(request, pk):
+    ulasan = get_object_or_404(Ulasan, pk=pk)
     form = UlasanForm(request.POST or None, instance=ulasan)
     
     if form.is_valid() and request.method == "POST":
         form.save()
         return HttpResponseRedirect(reverse('ulasan:show_ulasan'))
 
-    context = {'form': form}
+    katalog_items = Katalog.objects.all()
+    context = {
+        'form': form,
+        'ulasan': ulasan,
+        'katalog_items': katalog_items,
+    }
     return render(request, "edit_ulasan.html", context)
 
 def delete_ulasan(request, id):
+    
     ulasan = get_object_or_404(Ulasan, pk=id)
     ulasan.delete()
     return HttpResponseRedirect(reverse('ulasan:show_ulasan'))
