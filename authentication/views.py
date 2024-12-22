8f2785c18f3e0287d80c04d179937aa4db911c7f
DawnFall19 <ignasius.michael09@gmail.com>
2024-12-22 19:34:15 +0700

    fix register bug flutter

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
